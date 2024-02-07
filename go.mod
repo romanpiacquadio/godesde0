cc43e3a3243797807ad0ec733f0b3915c73d2aca
Roman Piacquadio <romanpiacquadio@hotmail.com>
2024-02-07 18:47:45 -0300

    Primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/romanpiacquadio/godesde0
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/romanpiacquadio/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
